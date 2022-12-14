657d593c4d5e053332f549b934f11ab7e251c1ac
Hihin Ruslan <ruslandh@altlinux.ru>
2022-12-14 22:22:21 +0300

    modified:   mount-tray.pro 	modified:   src/main.cpp 	new file:   translations/MountTray_ru.ts

--- mount-tray.pro
+++ mount-tray.pro
@@ -39,3 +39,17 @@ RESOURCES += \
 
 FORMS += \
     src/menudiskitem.ui
+
+TRANSLATIONS += translations/MountTray_ru.ts
+
+target.path = /usr/bin
+
+QMAKE_EXTRA_COMPILERS += lrelease
+lrelease.input  = TRANSLATIONS
+lrelease.output = ${QMAKE_FILE_BASE}.qm
+lrelease.commands = $$[QT_INSTALL_BINS]/lrelease ${QMAKE_FILE_IN} -qm translations/${QMAKE_FILE_BASE}.qm
+lrelease.CONFIG += no_link target_predeps
+translations.files = translations/MountTray_ru.qm
+
+translations.path = /usr/share/mount-tray/translations
+INSTALLS = target translations
---
+++ translations/MountTray_ru.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MenuDiskItem</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MountTrayApp</name>
+    <message>
+        <source>Empty</source>
+        <translation type="unfinished">Пустой</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished">О программе</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation type="unfinished">Выход</translation>
+    </message>
+    <message>
+        <source>Device connected: %1</source>
+        <translation type="unfinished">Подключено устройство %1</translation>
+    </message>
+    <message>
+        <source>Device removed: %1</source>
+        <translation type="unfinished">Удалено устройство %1</translation>
+    </message>
+    <message>
+        <source>Device &apos;%1&apos; is mounted to %2</source>
+        <translation type="unfinished">Устройство &apos;%1&apos; смонтировано к %2</translation>
+    </message>
+    <message>
+        <source>Device &apos;%1&apos; is unmounted
+Now you can eject USB Flash or CD/DVD Disk</source>
+        <translation type="unfinished">Устройство &apos;%1&apos; отмонтировано, теперь вы можете вытащить USB флешку или CD/DVD</translation>
+    </message>
+    <message>
+        <source>Can&apos;t mount device: %1
+%2</source>
+        <translation type="unfinished">Не могу смонтировать устройство: %1 %2</translation>
+    </message>
+    <message>
+        <source>Can&apos;t unmount device: %1
+%2</source>
+        <translation type="unfinished">Не могу отмонтировать устройство: %1 %2</translation>
+    </message>
+    <message>
+        <source>Device &apos;%1&apos; is unmounted</source>
+        <translation type="unfinished">Устройство &apos;%1&apos; отмонтировано</translation>
+    </message>
+    <message>
+        <source>About MountTray</source>
+        <translation type="unfinished">О MountTray</translation>
+    </message>
+    <message>
+        <source>&lt;h2&gt;About MountTray&lt;/h2&gt;&lt;p&gt;Version: </source>
+        <translation type="unfinished">&lt;h2&gt;О MountTray&lt;/h2&gt;&lt;p&gt;Версия: </translation>
+    </message>
+</context>
+</TS>
